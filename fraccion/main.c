0896159200bce7f954bb7b2f5442f61f20eefaea
zw29n47bmb-dev <zw29n47bmb@privaterelay.appleid.com>
2026-03-07 21:26:45 -0600

    Implement fraction creation, addition, and printing

--- fraccion/main.c
+++ fraccion/main.c
@@ -6,38 +6,33 @@ int main()
 
     printf("Prueba del TAD Fraccion\n");
 
-    /* TODO
-       1 Crear dos fracciones
-       ejemplo: 1/2 y 3/4
-    */
-    Fraccion* f1 = NULL;
-    Fraccion* f2 = NULL;
+    /* 1 Crear dos fracciones */
+    Fraccion* f1 = crearFraccion(1,2);
+    Fraccion* f2 = crearFraccion(3,4);
 
 
     printf("Fraccion 1: ");
-    /* TODO imprimir */
+    imprimir(f1);
 
 
     printf("Fraccion 2: ");
-    /* TODO imprimir */
+    imprimir(f2);
 
 
     printf("\nSuma de fracciones:\n");
 
-    /* TODO
-       2 Sumar fracciones
-    */
-    Fraccion* resultado = NULL;
+    /* 2 Sumar fracciones */
+    Fraccion* resultado = sumar(f1, f2);
 
 
-    /* TODO
-       3 Imprimir resultado
-    */
+    /* 3 Imprimir resultado */
+    imprimir(resultado);
 
 
-    /* TODO
-       4 Liberar memoria
-    */
+    /* 4 Liberar memoria */
+    destruir(f1);
+    destruir(f2);
+    destruir(resultado);
 
     return 0;
 }
