63e1ddc7b3d77f4549de56a251e28c1cc90b5b89
zw29n47bmb-dev <zw29n47bmb@privaterelay.appleid.com>
2026-03-07 21:18:04 -0600

    Update main.c

--- nodo/main.c
+++ nodo/main.c
@@ -5,26 +5,20 @@ int main()
 {
     printf("Prueba del TAD Nodo\n");
 
-    /* TODO
-       1 Crear un nodo con valor 10
-    */
-    Nodo* n = NULL;
+    /* 1 Crear un nodo con valor 10 */
+    Nodo* n = crearNodo(10);
 
-    /* TODO
-       2 Mostrar el valor del nodo usando obtenerValor
-    */
+    /* 2 Mostrar el valor del nodo usando obtenerValor */
+    printf("Valor del nodo: %d\n", obtenerValor(n));
 
-    /* TODO
-       3 Cambiar el valor a 25 usando asignarValor
-    */
+    /* 3 Cambiar el valor a 25 usando asignarValor */
+    asignarValor(n, 25);
 
-    /* TODO
-       4 Mostrar nuevamente el valor
-    */
+    /* 4 Mostrar nuevamente el valor */
+    printf("Nuevo valor del nodo: %d\n", obtenerValor(n));
 
-    /* TODO
-       5 Liberar el nodo con destruirNodo
-    */
+    /* 5 Liberar el nodo con destruirNodo */
+    destruirNodo(n);
 
     return 0;
 }
